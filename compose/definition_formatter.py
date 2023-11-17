13f7956b6135155220b47d9e2d81efa8842dd68c
torrua <torrua@users.noreply.github.com>
2023-11-17 21:25:27 +0700

    Apply Black Linter

--- compose/definition_formatter.py
+++ compose/definition_formatter.py
@@ -18,16 +18,21 @@ class DefinitionFormatter:
         Formats punctuation signs
         :return:
         """
-        to_key = '<k>'  # key
-        tc_key = '</k>'
-        to_log = '<l>'  # log
-        tc_log = '</l>'
+        to_key = "<k>"  # key
+        tc_key = "</k>"
+        to_log = "<l>"  # log
+        tc_log = "</l>"
 
-        return self.d.body \
-            .replace("<", "&lt;").replace(">", "&gt;") \
-            .replace("«", to_key).replace("»", tc_key) \
-            .replace("{", to_log).replace("}", tc_log) \
-            .replace("...", "…").replace("--", "—")
+        return (
+            self.d.body.replace("<", "&lt;")
+            .replace(">", "&gt;")
+            .replace("«", to_key)
+            .replace("»", tc_key)
+            .replace("{", to_log)
+            .replace("}", tc_log)
+            .replace("...", "…")
+            .replace("--", "—")
+        )
 
     def highlight_key(self, key: str, case_sensitive: bool = False) -> str:
         """
@@ -36,14 +41,16 @@ class DefinitionFormatter:
         :param case_sensitive:
         :return:
         """
-        to_key = '<k>'
-        tc_key = '</k>'
-        to_del = '<do_not_delete>'
-        tc_del = '</do_not_delete>'
+        to_key = "<k>"
+        tc_key = "</k>"
+        to_del = "<do_not_delete>"
+        tc_del = "</do_not_delete>"
         flag = re.IGNORECASE if not case_sensitive else 0
-        key_pattern = re.compile(f"{to_key}{re.escape(key.replace('*', '.*'))}{tc_key}", flags=flag)
+        key_pattern = re.compile(
+            f"{to_key}{re.escape(key.replace('*', '.*'))}{tc_key}", flags=flag
+        )
         def_body = key_pattern.sub(f"{to_del}\\g<0>{tc_del}", self.body_formatted)
-        def_body = def_body.replace(tc_key, '').replace(to_key, '')
+        def_body = def_body.replace(tc_key, "").replace(to_key, "")
         def_body = def_body.replace(to_del, to_key).replace(tc_del, tc_key)
 
         return def_body
@@ -57,11 +64,11 @@ class DefinitionFormatter:
         Returns:
 
         """
-        return tag % self.d.source_word.origin_x \
-            if (
-                self.d.source_word.origin_x and
-                self.d.source_word.type.group == "Cpx"
-        ) else str()
+        return (
+            tag % self.d.source_word.origin_x
+            if (self.d.source_word.origin_x and self.d.source_word.type.group == "Cpx")
+            else str()
+        )
 
     def tagged_word_name(self, tag: str) -> str:
         """
@@ -72,8 +79,11 @@ class DefinitionFormatter:
         Returns:
 
         """
-        return tag % self.d.source_word.name if not self.d.usage \
+        return (
+            tag % self.d.source_word.name
+            if not self.d.usage
             else tag % self.d.usage.replace("%", self.d.source_word.name)
+        )
 
     def tagged_definition_body(self, key: str, tag: str) -> str:
         """
--- compose/english_item.py
+++ compose/english_item.py
@@ -1,4 +1,3 @@
-
 from loglan_core.definition import BaseDefinition
 from loglan_core.event import BaseEvent
 from loglan_core.word import BaseWord
@@ -9,12 +8,11 @@ from compose.definition_formatter import DefinitionFormatter
 
 
 class EnglishItem(Item):
-
     def __init__(
-            self,
-            definitions: list[BaseDefinition],
-            key: str,
-            style: str = DEFAULT_HTML_STYLE,
+        self,
+        definitions: list[BaseDefinition],
+        key: str,
+        style: str = DEFAULT_HTML_STYLE,
     ):
         self.definitions = definitions
         self.key = key
@@ -22,24 +20,31 @@ class EnglishItem(Item):
 
     @staticmethod
     def select_definitions_by_key(
-            key: str, language: str = None,
-            case_sensitive: bool = False,
-            event_id: BaseEvent | int | str = None,
+        key: str,
+        language: str = None,
+        case_sensitive: bool = False,
+        event_id: BaseEvent | int | str = None,
     ) -> Select:
         return (
-            BaseDefinition
-            .by_key(key=key, language=language, case_sensitive=case_sensitive)
+            BaseDefinition.by_key(
+                key=key, language=language, case_sensitive=case_sensitive
+            )
             .join(BaseWord)
             .filter(BaseWord.filter_by_event_id(event_id=event_id))
             .order_by(BaseWord.name)
         )
 
     def export_as_html(self):
-        return '\n'.join([self.export_for_english(d, self.key, self.style) for d in self.definitions])
+        return "\n".join(
+            [self.export_for_english(d, self.key, self.style) for d in self.definitions]
+        )
 
     @staticmethod
-    def export_for_english(definition: BaseDefinition, key: str, style: str = DEFAULT_HTML_STYLE) -> str:
-
+    def export_for_english(
+        definition: BaseDefinition,
+        key: str,
+        style: str = DEFAULT_HTML_STYLE,
+    ) -> str:
         # de = definition english
         tags = {
             "normal": [
@@ -52,19 +57,39 @@ class EnglishItem(Item):
                 '<span class="w_origin">&lt;%s&gt;</span> ',
             ],
             "ultra": [
-                '(%s)', '[%s] ', ' %s', '<de>%s</de>',
-                '<ld>%s</ld>', '<wn>%s</wn>, ', '<o>&lt;%s&gt;</o> ', ],
+                "(%s)",
+                "[%s] ",
+                " %s",
+                "<de>%s</de>",
+                "<ld>%s</ld>",
+                "<wn>%s</wn>, ",
+                "<o>&lt;%s&gt;</o> ",
+            ],
         }
 
-        t_d_gram, t_d_tags, t_d_body, t_def, t_def_line, t_word_name, t_word_origin = tags[style]
+        (
+            t_d_gram,
+            t_d_tags,
+            t_d_body,
+            t_def,
+            t_def_line,
+            t_word_name,
+            t_word_origin,
+        ) = tags[style]
 
         gram_form = str(definition.slots or "") + definition.grammar_code
-        def_gram = t_d_gram % gram_form if gram_form else ''
-        def_tags = t_d_tags % definition.case_tags.replace("-", "&zwj;-&zwj;") if definition.case_tags else ''
+        def_gram = t_d_gram % gram_form if gram_form else ""
+        def_tags = (
+            t_d_tags % definition.case_tags.replace("-", "&zwj;-&zwj;")
+            if definition.case_tags
+            else ""
+        )
 
         def_body = DefinitionFormatter(definition).tagged_definition_body(key, t_d_body)
         word_name = DefinitionFormatter(definition).tagged_word_name(t_word_name)
-        word_origin_x = DefinitionFormatter(definition).tagged_word_origin_x(t_word_origin)
+        word_origin_x = DefinitionFormatter(definition).tagged_word_origin_x(
+            t_word_origin
+        )
 
-        definition = t_def % f'{def_tags}{def_gram}{def_body}'
-        return t_def_line % f'{word_name}{word_origin_x}{definition}'
+        definition = t_def % f"{def_tags}{def_gram}{def_body}"
+        return t_def_line % f"{word_name}{word_origin_x}{definition}"
